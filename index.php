a3a87e9fcd4ce803b42d55a84d4f0159deefd10d
Roger Monteiro <roger-15monteiro@hotmail.com>
2018-07-11 11:06:41 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <title>Crud FlexPeak</title>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+            <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        </head>
+        <body>
+            <div class="navbar-header">
+            <a class="navbar-brand" rel="home" href="/flex" title="Buy Sell Rent Everyting">
+            <img style="max-width:100px; margin-top: -7px;"
+                     src="./img/flexpeak.png">
+            </a>
+        </div>
+        </br>
+        </br>
+        </br>
+        </br>
+        </br>
+        <style>
+                .mesmo-tamanho {
+                    width: 15%;
+                    white-space: normal;
+                }
+            </style>
+            <center>
+            <div class="container">
+                <h2>Escolha abaixo:</h2></br>
+                <a class="btn btn-primary mesmo-tamanho" href="./aluno">ALUNOS</a>
+                <a class="btn btn-warning mesmo-tamanho" href="./curso">CURSOS</a>
+                <a class="btn btn-success mesmo-tamanho" href="./professor">PROFESSORES</a>
+            </div>
+        </ceter>
+        </body>
+    </html>
